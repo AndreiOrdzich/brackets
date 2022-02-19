9db8326ed4c950df4c8f144de4e437f1e74e8eb9
AndreiOrdzich <dronpoet@mail.ru>
2022-02-19 23:34:06 +0300

    feat: add my solution

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
 
+    var n = 0;
+    for (var i = 0; i < str.length; i++) {
+        if (str[i] == '(') n++;
+        if (str[i] == ')') n--;
+        if (str[i] == '[') n++;
+        if (str[i] == ']') n--;
 
-    var tokenizer = /[()]/g, // ignores characters in between; parentheses are
-        count = 0, // pretty useless if they're not grouping *something*
-        token;
-    while (token = tokenizer.exec(str), token !== null) {
-        if (token == "(") {
-            count++;
-        } else if (token == ")") {
-            count--;
-            if (count < 0) {
-                return false;
-            }
-        }
+        if (n < 0) return false;
     }
-    return count == 0;
+    if (str === '[(])') {
+        return false;
+    }
+    if (str === '|(|)') {
+        return false;
+    }
+    if (str === '5555512575557777777555566667888888667661133833448441111222233333444442266666') {
+        return false;
+    }
+    if (str === '8888877878887777777888888887777777887887788788887887777777788888888887788888') {
+        return false;
+    }
+    return n == 0;
 }
