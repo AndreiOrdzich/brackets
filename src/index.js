46f8267f562c9e18ca1c1f6488d2b9c83f8f576a
AndreiOrdzich <dronpoet@mail.ru>
2022-02-19 22:59:45 +0300

    feat: add my solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+
+    var tokenizer = /[()]/g, // ignores characters in between; parentheses are
+        count = 0, // pretty useless if they're not grouping *something*
+        token;
+    while (token = tokenizer.exec(str), token !== null) {
+        if (token == "(") {
+            count++;
+        } else if (token == ")") {
+            count--;
+            if (count < 0) {
+                return false;
+            }
+        }
+    }
+    return count == 0;
 }
